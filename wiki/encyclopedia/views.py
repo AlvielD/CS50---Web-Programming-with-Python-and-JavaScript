bf7893adb98105ba4e3497719ec3f0db10f41a0e
AlvielD <aesteban.task@gmail.com>
2022-06-21 16:58:48 +0200

    Added the error page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -13,19 +13,18 @@ def index(request):
 
 def render_entry(request, entry):
 
-    # Create an object to convert the markdown to HTML
-    markdowner = Markdown()
-
-    # Open file and read it, then, store in the context var
-    with open(f"entries/{entry}.md") as f:
-        page_content = markdowner.convert(f.read())
-    
     # Try to render the page if it exists, otherwise, display an error page
     try:
+        # Create an object to convert the markdown to HTML
+        markdowner = Markdown()
+
+        # Open file and read it, then, store in the context var
+        with open(f"entries/{entry}.md") as f:
+            page_content = markdowner.convert(f.read())
+    
         return render(request, f"encyclopedia/entry.html", {
             "entry": entry,
             "page_content": page_content
         })
     except:
-        # TODO Design an error web page for unexistence entries
-        return HttpResponse("The requested page was not found")
+        return render(request, "encyclopedia/error.html")
