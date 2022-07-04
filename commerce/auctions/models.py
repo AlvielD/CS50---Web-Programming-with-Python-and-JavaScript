ce99e8259782cdda8d0416447b7df1d3f0dc8086
AlvielD <aesteban.task@gmail.com>
2022-07-04 22:17:54 +0200

    Added the models for the AuctionListing, Bid and Comment

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,26 @@
+from turtle import title
+from unicodedata import category, name
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class AuctionListing():
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    startBid = models.IntegerField()
+    image = models.ImageField(upload_to='auction_images')
+    category = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Bid():
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+class Comment():
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=1000)
