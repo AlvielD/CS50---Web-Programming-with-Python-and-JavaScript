d62ae8545087126b6280511dda960dec00fb9c47
AlvielD <aesteban.task@gmail.com>
2022-07-05 19:45:35 +0200

    Fixed the first models and django's admin app

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class AuctionListing():
+class AuctionListing(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     startBid = models.IntegerField()
@@ -15,12 +15,12 @@ class AuctionListing():
     category = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Bid():
+class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
     amount = models.IntegerField()
 
-class Comment():
+class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     auction = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
     comment = models.CharField(max_length=1000)
