3e925aa601ccf20b09ddee3fc36bf1dcb79c12e3
Álvaro Esteban Muñoz <aesteban.task@gmail.com>
2023-02-07 11:16:40 +0100

    Now emails can be archived and unarchived. In addition, JSDocs were added.

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -11,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 });
 
+/**
+ * Show the compose view to send emails. Before, it removes the content
+ * of the forms' fields.
+ */
 function compose_email() {
 
   // Show compose view and hide other views
@@ -25,6 +29,11 @@ function compose_email() {
 }
 
 // TODO: Make the emails to light up when you set the mouse on them
+/**
+ * Load the view of the mails list depending on which mailbox
+ * we are requesting
+ * @param {string} mailbox - the mailbox to be loaded 
+ */
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
@@ -43,47 +52,105 @@ function load_mailbox(mailbox) {
       // Create the table element
       let email_table = document.createElement('table');
 
-      // For each email in the json object, insert a row on the table
+      // For each email in the json object, add it to the mails table
       emails.forEach(function(email) {
-        // Create the container for the email
-        let email_element = email_table.insertRow();      
-
-        // Set HTML text depending on the inbox
-        console.log(email)
-        if (mailbox == "sent") {
-          email_element.innerHTML = `<td><strong>To:</strong> ${email.recipients[0]}</td>`;
-        } else {
-          email_element.innerHTML = `<td><strong>From:</strong> ${email.sender}</td>`;
-        }
-        email_element.innerHTML += `<td><strong>Subject:</strong> ${email.subject}</td>`;
-        email_element.innerHTML += `<td style="text-align:right">${email.timestamp}</td>`;
-
-        // Add the event to make the email clickable
-        email_element.addEventListener('click', function() {
-          // Mark email as read
-          fetch(`/emails/${email.id}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                read: true
-            })
-          })
-          .then(response => response.json())
-          .then(result => {
-              // Print result
-              console.log("The email was read");
-          });
-
-          // Show the email view
-          show_mail(email);
-        });
-
-        // Add email to the emails-view
-        document.querySelector('#emails-view').append(email_table);
+        // Load the mail on the mails list
+        load_mail(mailbox, email_table, email);
       });
   });
-
 }
 
+/**
+ * This function loads a single mail in the mails listing that
+ * should be shown in the respective inbox.
+ * @param {string} mailbox - mailbox in which we are adding the mail
+ * @param {HTMLElement} email_table - the table that will contain all the emails
+ * @param {HTMLElement} email - mail object to be added to the list
+ */
+function load_mail(mailbox, email_table, email) {
+
+  // Create the container for the email
+  let email_element = email_table.insertRow(); // Row element of the table (will contain the email)
+  let arc_button = document.createElement('button');  // Button to archive or unarchive email
+
+  // Set HTML text depending on the mailbox
+  if (mailbox == "sent") {
+    // If the mailbox is "sent" -> set the field to "To:"
+    email_element.innerHTML = `<td><strong>To:</strong> ${email.recipients[0]}</td>`;
+  } else {
+    // If the mailbox is "inbox" or "archive" -> set to "From:"
+    email_element.innerHTML = `<td><strong>From:</strong> ${email.sender}</td>`;
+  }
+
+  // Set background color depending if its read or not
+  // by default is gray = read, so if its not read, set it to white
+  if (!email.read) {
+    email_element.style.backgroundColor = "white";
+  }
+
+  // Add the rest of the information to the inbox email (subject and timestamp)
+  email_element.innerHTML += `<td><strong>Subject:</strong> ${email.subject}</td>`;
+  email_element.innerHTML += `<td style="text-align:right">${email.timestamp}</td>`;
+
+  // EVENT TO MAKE MAIL CLICKABLE
+  email_element.addEventListener('click', function() {
+    // Mark email as read
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
+      })
+    })
+
+    // Show email view
+    show_mail(email);
+  });
+
+  // ADD AN EXTRA BUTTON IF WE ARE NOT IN THE "SENT" MAILBOX
+  if (mailbox != "sent") {
+    // Adjust some properties of the button and add it to the email
+    // Set the button depending if you can archive it or not
+    let cell_button = email_element.insertCell(); // Add new cell to the row
+    let archived_mail = false;
+
+    cell_button.style.textAlign = "center";       // Set button in the center of the cell
+    arc_button.classList = "btn btn-sm btn-outline-primary";
+
+    // Set the text of the button depending in which mailbox are we
+    if (mailbox == "inbox") {
+      arc_button.innerHTML = 'archive'; // Text displayed on button
+      archived_mail = true              // If we want to archive it, set this to true
+    } else {
+      arc_button.innerHTML = 'unarchive';// Text displayed on the button
+      // Here we don't set "archived_mail" since it is to false by default
+    }
+    cell_button.appendChild(arc_button);
+
+    // EVENT TO MAKE ARCHIVE BUTTON FUNCTIONABLE
+    arc_button.addEventListener('click', function(event) {
+      event.stopPropagation();
+      fetch(`/emails/${email.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: archived_mail
+        })
+      })
+      
+      // Refresh the mailbox
+      // Delay the refreshing for 5ms so it has time to update changes
+      setTimeout(load_mailbox, 5, mailbox);
+    });
+  }
+
+  // APPEND MAIL TO THE TABLE (FINALLY)
+  document.querySelector('#emails-view').append(email_table);
+}
+
+/**
+ * Shows a full view of the email with all its needed fields like
+ * "From", "To", "Timestamp", ...
+ * @param {HTMLElement} email - email to be shown in the view
+ */
 function show_mail(email) {
 
   // Show mailtext view and hide the others
@@ -119,6 +186,10 @@ function show_mail(email) {
   mailview.append(body_element);
 }
 
+/**
+ * Take the values from the form of the compose view and send a 
+ * POST request to the server. 
+ */
 function send_mail() {
   // Avoids the template from changing to the original one
   //event.preventDefault();
