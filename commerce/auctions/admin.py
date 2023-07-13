e8396edfa09c06bdd92bdb193fe5110f4bbacb13
Álvaro Esteban Muñoz <aesteban.task@gmail.com>
2023-07-13 14:08:52 +0200

    Deleted commerce folder - (USE THIS A STARTING POINT)

--- commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from auctions.models import User, AuctionListing, Bid, Comment
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(AuctionListing)
-admin.site.register(Bid)
-admin.site.register(Comment)
